7bb0feffee7b435ccb3f579c9c78727d02454525
Jake Bradford <jacob.bradford@connect.qut.edu.au>
2020-06-02 10:53:25 +1000

    - updated index to include precalculated scores

--- index_offtargetSites.cpp
+++ index_offtargetSites.cpp
@@ -21,6 +21,7 @@ g++ -o index_offtargetSites index_offtargetSites.cpp -O3 -std=c++11 -fopenmp -mp
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <map>
 
 using namespace std;
 
@@ -52,6 +53,80 @@ string signatureToSequence(uint64_t signature)
         sequence[j] = signatureIndex[(signature >> (j * 2)) & 0x3];
     }
     return sequence;
+}
+
+// combinations
+vector<uint64_t> computeMasksTwoBit(int seqLength, int mismatches) {
+	vector<uint64_t> masks;
+	
+	// there are more positions than mismatches
+	if (mismatches < seqLength) {
+		
+		if (mismatches > 0) {
+			// some mismatches across a long sequence
+			for (auto mask : computeMasksTwoBit(seqLength - 1, mismatches -1)) {
+				// orig: masks.push_back((1 << (seqLength - 1)) + mask);
+				masks.push_back((1LLU << (seqLength - 1)*2) + mask);
+			}
+			for (auto mask : computeMasksTwoBit(seqLength - 1, mismatches)) {
+				masks.push_back(mask);
+			}
+		} else {
+			// no mismatches at all
+			masks.push_back(0LLU);
+		}
+		
+	// mismatches >= seqLength
+	// every position is going to be a mismatch
+	// eg: mismatches=4 and seqLength=4, we want: 10 10 10 10
+	} else {
+		// orig: masks.push_back((1LLU << seqLength) - 1);
+		uint64_t tempMask = 0;
+		for (int i = 0; i < seqLength; i++) {
+			tempMask |= (1LLU << i*2);
+		}
+		masks.push_back(tempMask);
+	}
+	return masks;
+}
+
+double single_score(int* mismatch_array, int length) {
+    int i;
+    double T1=1.0, T2, T3, d=0.0, score;
+    double M[] = {0.0, 0.0, 0.014, 0.0, 0.0, 0.395, 0.317, 0.0, 0.389, 0.079, 0.445, 0.508, 0.613, 0.851, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583};
+
+    /* 1st term */
+    for(i=0; i<length; ++i)
+            T1 = T1*(1.0-M[mismatch_array[i]]);
+
+    /* 2nd term */
+    if(length==1)
+            d = 19.0;
+    else {
+            for(i=0; i<length-1; ++i)
+                    d += mismatch_array[i+1]-mismatch_array[i];
+            d = d/(length-1);
+    }
+    T2 = 1.0 / ((19.0-d)/19.0 * 4.0 + 1);
+
+    /* 3rd term */
+    T3 = 1.0 / (length*length);
+
+    /* Total score */
+    score = T1*T2*T3*100;
+    return score;
+}
+
+double sscore(uint64_t xoredSignatures)
+{
+    int mismatch_array[20], m = 0;
+    for (size_t j = 0; j < seqLength; j++) {
+        if ((xoredSignatures >> (j * 2)) & 0x3) {
+            mismatch_array[m++] = j;
+        }
+    }
+    if (m == 0) return 0.0;
+    return single_score(mismatch_array, m);
 }
 
 int main(int argc, char **argv)
@@ -123,7 +198,7 @@ int main(int argc, char **argv)
 			
 			myCount++;
 			if (myCount % 10000 == 0) {
-				fprintf(stderr, "%zu/%zu : %i\n", myCount, seqCount, i);
+				fprintf(stderr, "%zu/%zu : %zu\n", myCount, seqCount, i);
 			}
 			
 			i += occurrences;
@@ -131,10 +206,10 @@ int main(int argc, char **argv)
     
     }
 	printf("Finished counting occurrences, now constructing index...\n");
-    size_t sliceWidth = atoi(argv[3]); // 8
-    size_t sliceLimit = 1 << sliceWidth; // 256
-    size_t sliceCount = (seqLength * 2) / sliceWidth; // 5
-    size_t offtargetsCount = myCount; //seqSignatures.size();
+    size_t sliceWidth = atoi(argv[3]);
+    size_t sliceLimit = 1 << sliceWidth;
+    size_t sliceCount = (seqLength * 2) / sliceWidth;
+    size_t offtargetsCount = myCount;
 	
     vector<vector<vector<uint64_t>>> sliceLists(sliceCount, vector<vector<uint64_t>>(sliceLimit));
     
@@ -147,7 +222,6 @@ int main(int argc, char **argv)
         
 		uint32_t signatureId = 0;
         for (uint64_t signature : seqSignatures) {
-			//uint16_t occurrences = (uint16_t)(signature >> (seqLength * 2));
 			uint32_t occurrences = seqSignaturesOccurrences[signatureId];
             uint8_t sliceVal = (signature & sliceMask) >> sliceShift;
 			
@@ -157,18 +231,45 @@ int main(int argc, char **argv)
         }
     }
     
-	printf("Finished constructing index, now writing to file...\n");
+	printf("Finished constructing index, now precalculating scores...\n");
+	
+	// Precalculate all the scores
+	map<uint64_t, double> precalculatedScores;
+	
+	int maxDist = seqLength * 2 / sliceWidth - 1;
+	size_t scoresCount = 0;
+	
+	#pragma omp parallel for
+	for (int i = 1; i <= maxDist; i++) {
+		vector<uint64_t> tempMasks;
+		tempMasks = computeMasksTwoBit(20, i);
+		for (auto mask : tempMasks) {
+			double score = sscore(mask);
+			precalculatedScores.insert(pair<uint64_t, double>(mask, score));
+			scoresCount++;
+		}
+	}
+	
+	printf("Finished calculating scores, now preparing to write to disk...\n");
+	
     fp = fopen(argv[4], "wb");
-    
     vector<size_t> slicelistHeader;
     slicelistHeader.push_back(offtargetsCount);
     slicelistHeader.push_back(seqLength);
     slicelistHeader.push_back(seqCount);
     slicelistHeader.push_back(sliceWidth);
     slicelistHeader.push_back(sliceCount);
+    slicelistHeader.push_back(scoresCount);
+	
 	
 	// write the header
     fwrite(slicelistHeader.data(), sizeof(size_t), slicelistHeader.size(), fp);
+
+	// write the precalculated scores
+	for (auto const& x : precalculatedScores) {
+		fwrite(&x.first, sizeof(uint64_t), 1, fp);
+		fwrite(&x.second, sizeof(double), 1, fp);
+	}
 
 	// write the offtargets
 	fwrite(seqSignatures.data(), sizeof(uint64_t), seqSignatures.size(), fp);
@@ -179,11 +280,14 @@ int main(int argc, char **argv)
             fwrite(&sz, sizeof(size_t), 1, fp);
         }
     }
+
     for (size_t i = 0; i < sliceCount; i++) { // 5
         for (size_t j = 0; j < sliceLimit; j++) { // 256
             fwrite(sliceLists[i][j].data(), sizeof(uint64_t), sliceLists[i][j].size(), fp); // vector
         }
     }
+	
+	printf("Writing to disk...\n");
     
     fclose(fp);
     printf("Done.\n");
