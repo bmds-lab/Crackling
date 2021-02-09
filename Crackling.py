e16208e48546a8efd4d3d8c4f92ce82c7100bcde
Jake Bradford <jacob.bradford@connect.qut.edu.au>
2021-02-09 12:48:18 +1000

    dev-v1.1.0
    - refactored Crackling.py: moved helper functions into their own file (Helpers.py)

--- Crackling.py
+++ Crackling.py
@@ -7,52 +7,13 @@ Config:
     - See config.ini
 '''
 
-import sys, argparse, os, re, joblib, timeit, ast, glob, string, random, json, time, glob, csv
+import argparse, ast, csv, joblib, os, re, sys, time
 from sklearn.svm import SVC
-from subprocess import call
-from time import localtime, strftime, gmtime
 
 from ConfigManager import ConfigManager
 from Paginator import Paginator
 from Constants import *
-
-
-# Function that returns the reverse-complement of a given sequence
-def rc(dna):
-    complements = str.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
-    rcseq = dna.translate(complements)[::-1]
-    return rcseq
-
-
-# Function that replaces U with T in the sequence (to go back from RNA to DNA)
-def transToDNA(rna):
-    switch_UT = str.maketrans('U', 'T')
-    dna = rna.translate(switch_UT)
-    return dna
-
-
-# Function that calculates the AT% of a given sequence
-def AT_percentage(seq):
-    total = 0.0
-    length = float(len(seq))
-    for c in seq:
-        if c in "AT":
-            total += 1
-    return 100.0*total/length
-
-
-def printer(stringFormat):
-    print('>>> {}:\t{}\n'.format(
-        strftime("%Y-%m-%d %H:%M:%S", localtime()),
-        stringFormat
-    ))
-
-
-def caller(*args, **kwargs):
-    printer(f"| Calling: {args}")
-    call(*args, **kwargs)
-    printer(f"| Finished")
-
+from Helpers import * 
 
 def Crackling(configMngr):
     totalSizeBytes = configMngr.getDatasetSizeBytes()
---
+++ Helpers.py
@@ -0,0 +1,38 @@
+from time import localtime, strftime, gmtime
+from subprocess import call
+
+# Function that returns the reverse-complement of a given sequence
+def rc(dna):
+    complements = str.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
+    rcseq = dna.translate(complements)[::-1]
+    return rcseq
+
+
+# Function that replaces U with T in the sequence (to go back from RNA to DNA)
+def transToDNA(rna):
+    switch_UT = str.maketrans('U', 'T')
+    dna = rna.translate(switch_UT)
+    return dna
+
+
+# Function that calculates the AT% of a given sequence
+def AT_percentage(seq):
+    total = 0.0
+    length = float(len(seq))
+    for c in seq:
+        if c in "AT":
+            total += 1
+    return 100.0*total/length
+
+
+def printer(stringFormat):
+    print('>>> {}:\t{}\n'.format(
+        strftime("%Y-%m-%d %H:%M:%S", localtime()),
+        stringFormat
+    ))
+
+
+def caller(*args, **kwargs):
+    printer(f"| Calling: {args}")
+    call(*args, **kwargs)
+    printer(f"| Finished")
