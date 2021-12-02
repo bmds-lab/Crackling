e55722e75aac30465c1ce7156e71664e5eef18a0
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2021-12-02 11:44:08 +1000

    - Removed redundant functions and lines from Helpers.py

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,5 @@
-from time import localtime, strftime, gmtime
-from subprocess import call, run
-import datetime, tempfile
+from subprocess import run
+from datetime import datetime
 
 # Function that returns the reverse-complement of a given sequence
 def rc(dna):
@@ -26,93 +25,16 @@ def AT_percentage(seq):
     return 100.0*total/length
 
 
+# Function that formats provided text with time stamp
 def printer(stringFormat):
     print('>>> {}:\t{}\n'.format(
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f"),
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f"),
         stringFormat
     ))
 
 
-def caller(*args, **kwargs):
-    printer(f"| Calling: {args}")
-    call(*args, **kwargs)
-    printer(f"| Finished")
-
-
+# Function that runs given external call and records start and finish times using printer
 def runner(*args, **kwargs):
     printer(f"| Calling: {args}")
     run(*args, **kwargs)
     printer(f"| Finished")
-
-
-def explodeMultiFastaFile(fpInput, fpOutputTempDir):
-    newFilesPaths = []
-
-    with open(fpInput, 'r') as fRead:
-        fWrite = None
-
-        for line in fRead:
-            line = line.strip()
-            
-            # just found a new fasta segment. open a new file
-            if line[0] == '>':
-                fpTemp = tempfile.NamedTemporaryFile(
-                    mode = 'w+', 
-                    delete = False,
-                    dir = fpOutputTempDir
-                )
-                
-                # close the current file if necessary
-                if fWrite is not None:
-                    fWrite.write('\n')
-                    fWrite.close()
-                    
-                # open a new one
-                fWrite = open(fpTemp.name, 'w+')
-                newFilesPaths.append(fpTemp.name)
-                
-                fWrite.write(line)
-                fWrite.write('\n')
-
-            if line[0] != '>':
-                fWrite.write(line.upper().strip())
-        
-        # close the last file if necessary
-        if fWrite is not None:
-            fWrite.close()
-            
-    return newFilesPaths
-
-def encodeString(guideString):
-    A = 0b00
-    T = 0b01
-    G = 0b10
-    C = 0b11
-    bitEncoded = 0b0
-    for base in guideString:
-        bitEncoded = bitEncoded<<2
-        if base == 'A':
-            bitEncoded = bitEncoded | A
-        elif base == 'T':
-            bitEncoded = bitEncoded | T
-        elif base == 'G':
-            bitEncoded = bitEncoded | G
-        elif base == 'C':
-            bitEncoded = bitEncoded | C
-    return bitEncoded
-
-def decode2Bit(guideString):
-    bitMask = 0b11
-    stringDecoded = ''
-    for i in range(23):
-        decodedChar = guideString & bitMask
-        if decodedChar == 0b00:
-            stringDecoded = 'A' + stringDecoded
-        elif decodedChar == 0b01:
-            stringDecoded = 'T' + stringDecoded
-        elif decodedChar == 0b10:
-            stringDecoded = 'G' + stringDecoded
-        elif decodedChar == 0b11:
-            stringDecoded = 'C' + stringDecoded
-        guideString = guideString>>2
-    return stringDecoded
