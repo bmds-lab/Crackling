b4add34a1bb44fb62d018970fb2153a22d01c02a
Jake Bradford <jake.bradford@qut.edu.au>
2024-01-11 14:14:40 +1000

    more work needed here

--- src/crackling/utils/extractOfftargets.py
+++ src/crackling/utils/extractOfftargets.py
@@ -15,7 +15,7 @@ To use:     python3.7 ExtractOfftargets.py output-file  (input-files... | input-
 
 '''
 
-import glob, multiprocessing, os, re, shutil, sys, tempfile, heapq, argparse
+import glob, multiprocessing, os, re, sys, tempfile, heapq, psutil, argparse
 from crackling.Helpers import *
 from crackling.Paginator import Paginator
 
@@ -23,7 +23,7 @@ from crackling.Paginator import Paginator
 pattern_forward_offsite = r"(?=([ACG][ACGT]{19}[ACGT][AG]G))"
 pattern_reverse_offsite = r"(?=(C[CT][ACGT][ACGT]{19}[TGC]))"
 
-def explodeMultiFastaFile(fpInput, fpOutputTempDir):
+def _explodeMultiFastaFile(fpInput, fpOutputTempDir):
     newFilesPaths = []
 
     with open(fpInput, 'r') as fRead:
@@ -61,7 +61,31 @@ def explodeMultiFastaFile(fpInput, fpOutputTempDir):
             
     return newFilesPaths
 
-def processingNode(fpInputs, fpOutputTempDir = None):
+def _splitListOfFilesBySize(listOfFiles, maxBlockSize=None, sizeFactor=1.0):
+
+    if maxBlockSize == None:
+        maxBlockSize = psutil.virtual_memory().available # bytes
+
+    pages = [[]]
+    
+    bufferedSizes = [os.path.getsize(x) * sizeFactor for x in listOfFiles]
+
+    for fileNumber, bufferedSize in enumerate(bufferedSizes):
+        predictedBlockSize = sum([
+            bufferedSizes[i]
+            for i in pages[-1]
+        ], bufferedSize)
+        
+        if predictedBlockSize >= maxBlockSize and len(pages[-1]) != 0:
+            pages.append([])
+        pages[-1].append(fileNumber)
+        
+    return [
+        [listOfFiles[i] for i in page]
+        for page in pages
+    ]
+
+def _processingNode(fpInputs, fpOutputTempDir = None):
     # Create a temporary file
     fpTemp = tempfile.NamedTemporaryFile(
         mode = 'w+', 
@@ -90,6 +114,7 @@ def processingNode(fpInputs, fpOutputTempDir = None):
                         seqsByHeader[header].append(line.rstrip().upper())
 
             # For each FASTA sequence
+            lineNumber = 0
             offtargets = [] 
             for header in seqsByHeader:
                 seq = ''.join(seqsByHeader[header])
@@ -104,50 +129,53 @@ def processingNode(fpInputs, fpOutputTempDir = None):
                         offtargets.append(
                             seqModifier(match_chr[i][0:20])
                         )
+
+                lineNumber += 1
                 
             outFile.write(''.join(f'{offTarget}\n' for offTarget in offtargets))
-            return len(offtargets)
 
-# Node function that sorts a file for multiprocessing pool
-def sortingNode(fileToSort, sortedTempDir):
-    # Create a temporary file
-    sortedFile = tempfile.NamedTemporaryFile(
-        mode = 'w+', 
-        delete = False,
-        dir = sortedTempDir
-    )
-    # Sort input file and store in new output dir
-    with open(fileToSort, 'r') as input:
-        # Read 'page'
-        page = input.readlines()
-        # Sort Page
-        page.sort()
-        # Write sorted page to file
-        sortedFile.writelines(page)
-        # Close sorted file
-        sortedFile.close()
-
-def paginatedSort(filesToSort, fpOutput, mpPool, maxNumOpenFiles=400): 
+def _sortSites(filesToSort, fpOutput, mpPool): 
+    printer('Preparing for ISSL by sorting all intermediate files')
+    printer('Then, writing to user-specified output file')
+    
     # Create temp file directory
     sortedTempDir = tempfile.TemporaryDirectory()
     printer(f'Created temp directory {sortedTempDir.name} for sorting')
 
-    # Generate args for sorting 
-    args = [
-        (
-            file,
-            sortedTempDir.name 
-        ) for file in filesToSort
-    ]
+    # If the input is a directory then select all files
+    if os.path.isdir(filesToSort):
+        filesToSort = glob.glob(
+            os.path.join(
+                filesToSort, 
+                '*'
+            )
+        )
+        
+    # Split sorting the files into pages to avoid saturating memory
+    # Sizefactor: in Python, 70 bytes are needed for a 20 character string
+    pages = _splitListOfFilesBySize(filesToSort, maxBlockSize=None, sizeFactor=4.0)
+    
+    if len(pages) > 1:
+        printer(f'Files will be sorted in {len(pages)} pages to avoid saturating memory')
 
-    # Submit job to multiprocessing pool
-    mpPool.starmap(
-        sortingNode,
-        args
-    )
-    
-    printer('Sorting of each file completed')
-    
+    for i, page in enumerate(pages):
+        if len(pages) > 1:
+            printer(f'Starting on page {i+1} of {len(pages)}, containing {len(page)} files')
+        else:
+            printer(f'Beginning to sort {len(page)} files')
+
+        args = [
+            (
+                file,
+                sortedTempDir.name 
+            ) for file in page
+        ]
+
+        mpPool.starmap(
+            _sortingNode,
+            args
+        )
+        
     # Collect sorted files to merge
     sortedFiles = glob.glob(
         os.path.join(
@@ -156,48 +184,43 @@ def paginatedSort(filesToSort, fpOutput, mpPool, maxNumOpenFiles=400):
         )
     )
     
+    printer(f'Beginning to merge {len(sortedFiles)} sorted files')
+    
     # Open all the sorted files to merge
-    printer(f'Beginning to merge sorted files, {maxNumOpenFiles:,} at a time')
-    while len(sortedFiles) > 1:
-        # A file to write the merged sequences to
-        mergedFile = tempfile.NamedTemporaryFile(delete = False)
-        
-        # Select the files to merge
-        while True:
-            try:
-                sortedFilesPointers = [open(file, 'r') for file in sortedFiles[:maxNumOpenFiles]]
-                break
-            except OSError as e:
-                if e.errno == 24:
-                    printer(f'Attempted to open too many files at once (OSError errno 24)')
-                    maxNumOpenFiles = max(1, int(maxNumOpenFiles / 2))
-                    printer(f'Reducing the number of files that can be opened by half to {maxNumOpenFiles}')
-                    continue
-                raise e
-                    
-        printer(f'Merging {len(sortedFilesPointers):,}')
-        
-        # Merge and write
-        with open(mergedFile.name, 'w') as f:
-            f.writelines(heapq.merge(*sortedFilesPointers))
-        
-        # Close all of the open files
-        for file in sortedFilesPointers:
-            file.close()
-          
-        # prepare for the next set to be merged
-        sortedFiles = sortedFiles[maxNumOpenFiles:] + [mergedFile.name]
-    
-    shutil.move(mergedFile.name, fpOutput)
+    sortedFilesPointers = [open(file, 'r') for file in sortedFiles]
 
-def startMultiprocessing(fpInputs, fpOutput, mpPool, numThreads, maxOpenFiles):
-    printer('Extracting off-targets using multiprocessing approach')
+    # Merge all the sorted files 
+    with open(fpOutput, 'w') as f:
+        f.writelines(heapq.merge(*sortedFilesPointers))
     
-    printer(f'Allowed processes: {numThreads}')
+    # Close all of the sorted files
+    for file in sortedFilesPointers:
+        file.close()
+
+def _sortingNode(fileToSort, sortedTempDir):
+    # Create a temporary file
+    sortedFile = tempfile.NamedTemporaryFile(
+        mode = 'w+', 
+        delete = False,
+        dir = sortedTempDir
+    )
+    # Sort input file and store in new output dir
+    with open(fileToSort, 'r') as input:
+        lines = input.readlines()
+        lines.sort()
+        sortedFile.writelines(lines)
+        sortedFile.close()
+
+def _startProcessing(fpInputs, fpOutput, numProcessors, sizeFactor=1.0):
+    printer('Extracting off-targets using multiprocessing approach')
+    printer(f'Allowed processes: {numProcessors} (system has {os.cpu_count()})')
+    
+    mpPool = multiprocessing.Pool(numProcessors)
     
     fpTempDir = tempfile.TemporaryDirectory()
     printer(f'Created a temporary directory for intermediate files: {fpTempDir.name}')
 
+    # Find all files in the input directory (if not a list of files)
     if len(fpInputs) == 1 and os.path.isdir(fpInputs[0]):
         fpInputs = glob.glob(
             os.path.join(
@@ -205,92 +228,100 @@ def startMultiprocessing(fpInputs, fpOutput, mpPool, numThreads, maxOpenFiles):
                 '*'
             )
         )
-
-    if len(fpInputs) == 1:
-        printer('Only one input file to process')
         
+    # If there is only one file to process, split it up
+    if len(fpInputs) == 1:
         fpExplodeTempDir = tempfile.TemporaryDirectory()
+        
+        printer('Only one input file to process')
         printer('Attempting to explode multi-FASTA file')
         printer(f'Writing each file to a temporary directory: {fpExplodeTempDir.name}')
-        
-    
-        fpInputs = explodeMultiFastaFile(
+
+        fpInputs = _explodeMultiFastaFile(
             fpInputs[0],
             fpExplodeTempDir.name
         )
         
         printer(f'Exploded into {len(fpInputs)} files')
-
-    args = [
-        (
-            [fpInput],
-            fpTempDir.name 
-        ) for fpInput in fpInputs
-    ]
-
-    printer(f'Beginning to process {len(args)} files')
-
-    numTargets = mpPool.starmap(
-        processingNode,
-        args
-    )
-
-    printer(f'Processing completed. Found {sum(numTargets):,} targets.')
     
-    printer('Preparing for ISSL by sorting all intermediate files')
+    else:
+        printer(f'Processing {len(fpInputs)} files')
+
+    # Split processing the files into pages to avoid saturating memory
+    pages = _splitListOfFilesBySize(fpInputs, sizeFactor=sizeFactor)
     
-    # sort in batches
-    paginatedSort(
-        glob.glob(
-            os.path.join(
-                fpTempDir.name, 
-                '*'
-            )
-        ), 
-        fpOutput,
-        mpPool,
-        maxNumOpenFiles=maxOpenFiles
-    )
+    if len(pages) > 1:
+        printer(f'Files will be multiprocessed in {len(pages)} pages to avoid saturating memory')
 
-def main():
-    parser = argparse.ArgumentParser(description='Extract CRISPR target sites for Crackling.')
+    for i, page in enumerate(pages):
+        if len(pages) > 1:
+            printer(f'Starting on page {i+1} of {len(pages)}, containing {len(page)} files')
+        else:
+            printer(f'Beginning to process {len(page)} files')
+
+        print(page)
+
+        args = [
+            (
+                [fpInput],
+                fpTempDir.name 
+            ) for fpInput in page
+        ]
     
-    parser.add_argument('output',
-        help='A file to write the off-targets to.'
-    )
-    parser.add_argument('inputs',
-        help='A space-separated list of paths or a path containing wildcards to FASTA files.',
-        nargs='+'
-    )
-    parser.add_argument('--maxOpenFiles', 
-        help='The number of files allowed to be opened by a single process. Use `ulimit -n` to find out your system setting.', 
-        type=int,
-        default=1000, 
-        required=False
-    )
-    parser.add_argument('--threads', 
-        help='The number of threads to use. Default is `os.cpu_count()`.', 
-        type=int,
-        default=os.cpu_count(), 
-        required=False
-    )
-
-    args = parser.parse_args()
-
-    # Create multiprocessing pool
-    # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.pool.Pool.starmap
-    mpPool = multiprocessing.Pool(args.threads)
-
-    startMultiprocessing(
-        args.inputs, 
-        args.output, 
-        mpPool, 
-        args.threads, 
-        args.maxOpenFiles
-    )
     
-    # Clean up. Close multiprocessing pool
+        mpPool.starmap(
+            _processingNode,
+            args
+        )
+
+    printer('Processing completed')
+    
+    _sortSites(fpTempDir.name, fpOutput, mpPool)
+    
     mpPool.close()
+
+def extractOfftargets(output, inputs, memory=None, numProcessors=None, sizeFactor=None):
+    if memory is None:
+        memory = psutil.virtual_memory().available
+        
+    if numProcessors is None:
+        numProcessors = os.cpu_count()
+        
+    if sizeFactor is None:
+        '''
+        sizeFactor is defaulted to 13.0
+        
+        Experiments using artificial input sequences with lengths between 10k
+        and 300k show that approx. 13x the memory of the input sequence is 
+        needed. i.e. the number of sites * 70 bytes (50 overhead + 20 seq bytes)
+        
+        Based on the 12 genomes used in (Bradford, 2022; CRISPR Journal), this
+        experimental approximation matches real data.
+        '''
+        sizeFactor = 30.0
+        
+    _startProcessing(inputs, output, numProcessors, sizeFactor=sizeFactor)
+    
+def main():
+    parser = argparse.ArgumentParser(description='Extract CRISPR-Cas9 target sites. Used primarily for creating an ISSL index of off-target sites.')
+    parser.add_argument('output', help='The output file')
+    parser.add_argument('inputs', help='The FASTA-formatted files to extract sites from', nargs='+')
+    parser.add_argument('-m', '--memory', required=False, default=None,
+        help='The maximum volume of memory to consume. Adjust `--sizeFactor` to scale the effective volume of memory available. Note, if one file exceeds the available memory, it will be processed by itself. If not specified, the current available volume of memory will be used.')
+    parser.add_argument('-p', '--processors', required=False, default=None, type=int,
+        help='The number of CPU cores to use in multiprocessing. Default is all cores.')
+    parser.add_argument('--sizefactor', required=False, default=None, type=float,
+        help='Input file sizes will be multiplied by this value. Larger values imply fewer files will be processed simultaneously. Note: Python has an overhead on strings, of 50 bytes; therefore, each extracted site (20 bp) needs 70 bytes in memory.')
+    
+    args = parser.parse_args()
+    
+    extractOfftargets(
+        args.output,
+        args.inputs,
+        memory=args.memory,
+        numProcessors=args.processors,
+        sizeFactor=args.sizefactor
+    )
 
     printer('Goodbye.')
 
